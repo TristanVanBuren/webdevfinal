95c7041298b501c4260dc92d7729319b552297fe
TristanVanBuren <twvanbu1@gmail.com>
2024-11-19 14:51:22 -0500

    10min autosave wip

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Farm Management</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <!-- Hero Section -->
+    <div class="hero-section">
+        <h1 class="hero-title">High top Mountain farm management sowftware</h1>
+        <p class="hero-subtitle">" why paper when we can have it centralized online! "</p>
+    
+        
+    <!-- Search moved to hero section -->
+    <div class="hero-search">
+        <h2>farming is hard and we are here to make it simple for you please make a selection below to start</h2>
+    </div>
+    </div>
+
+<!--going to put nav flex box here-->
+
+</body>
